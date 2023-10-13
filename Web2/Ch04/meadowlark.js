3c773d355dc25be610d4fce622b5f1d8120d9faf
Parksiwoo-04 <127005251+Parksiwoo-04@users.noreply.github.com>
2023-10-13 10:03:53 +0900

    Add files via upload

---
+++ Web2/Ch04/meadowlark.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const expresshandlebars = require('express-handlebars')
+const app = express()
+const port = process.env.PORT || 3000
+const fortune = require('./lib/fortune')
+
+app.engine('handlebars',expresshandlebars({
+    defaultlayout: 'main',
+}))
+app.set('view engine','handlebars')
+app.use(express.static(__dirname + '/public'))
+
+app.get('/',(req,res)=>{
+    res.render('about',{fortune: fortune.feFortune()})
+})
+// custom 404 page
+app.use((req, res)=> {
+    res.status(404)
+    res.render('404')
+})
+//custom 500 page
+app.use((err, req, res, next)=> {
+    console.error(err.message)
+    res.status(500)
+    res.render('500')
+})
+
+app.listen(port, ()=> console.log(
+    `Express started on http://localhost:${port}; `+
+    `press Ctrl-C to terminate...`))
